6dd72325f75895f171d3913635dc6f5c195253b5
jonathangrenader <143565278+jonathangrenader@users.noreply.github.com>
2025-10-03 18:23:28 -0300

    Primera versión de la web Beraka Toldos

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    // Cierra el menú al hacer clic en un enlace (para móviles)
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+            }
+        });
+    });
+});
